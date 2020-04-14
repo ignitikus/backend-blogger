efee60ece9168fe2e618d2dd3eadafe9a867c4d3
NikolayK <nikolay.kim@codeimmersive.com>
2020-04-14 10:54:40 -0400

    creates backend and 5 routes

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next)=> {
+  res.json('All blogs')
+});
+
+router.get('/singleblog', (req, res, next)=> {
+  res.json('Single blog')
+});
+
+router.post('/createblog', (req,res,next) => {
+  res.json('create a blog')
+})
+
+router.put('/updateblog', (req,res,next) => {
+  res.json('update a blog')
+})
+
+router.delete('/deleteblog', (req,res,next) => {
+  res.json('delete a blog')
+}
+)
+
+
+
+module.exports = router;
